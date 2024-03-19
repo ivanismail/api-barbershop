9125cfd661366a8f481f782276cec9ca55d5f12a
Ivan Ismail <ivan.ismail@misdm.com>
2024-03-19 13:56:56 +0700

    add: php artisan make:seeder UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         // insert data ke table user
+         DB::table('users')->insert([
+        	'name' => 'Ivan Ismail',
+        	'phone_number' => '+628123456789',
+            'password' => '$2y$10$9AvuQce5sU0TJLRRz80Nje3QJlRVZEgmIHGFXhJccc0pFtQzIGAP6',
+            'active' => true,
+        ]);
+    }
+}
