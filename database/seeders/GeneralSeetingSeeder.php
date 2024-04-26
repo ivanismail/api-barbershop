20da4c0e9cea37302a2bc407c36c4e6843080388
Ivan Ismail <ivan.ismail@misdm.com>
2024-04-26 16:09:28 +0700

    add: gs point

--- database/seeders/GeneralSeetingSeeder.php
+++ database/seeders/GeneralSeetingSeeder.php
@@ -24,7 +24,8 @@ class GeneralSeetingSeeder extends Seeder
             ['code' => 'STS', 'value' => '4', 'value_2' => '', 'description' => 'Selesai', 'active' => true],
             ['code' => 'STS', 'value' => '5', 'value_2' => '', 'description' => 'Batal', 'active' => true],
             ['code' => 'PAYMENT', 'value' => '1', 'value_2' => '', 'description' => 'Tunai', 'active' => true],            
-            ['code' => 'PAYMENT', 'value' => '2', 'value_2' => '', 'description' => 'GOPAY', 'active' => true],
+            ['code' => 'PAYMENT', 'value' => '2', 'value_2' => '', 'description' => 'Poin', 'active' => true],
+            ['code' => 'PAYMENT', 'value' => '3', 'value_2' => '', 'description' => 'Gopay', 'active' => false],
             ['code' => 'schedule', 'value' => '9', 'value_2' => '', 'description' => '09:00', 'active' => true],
             ['code' => 'schedule', 'value' => '10', 'value_2' => '', 'description' => '10:00', 'active' => true],
             ['code' => 'schedule', 'value' => '11', 'value_2' => '', 'description' => '11:00', 'active' => true],
@@ -35,6 +36,7 @@ class GeneralSeetingSeeder extends Seeder
             ['code' => 'schedule', 'value' => '17', 'value_2' => '', 'description' => '17:00', 'active' => true],
             ['code' => 'schedule', 'value' => '19', 'value_2' => '', 'description' => '19:00', 'active' => true],
             ['code' => 'schedule', 'value' => '20', 'value_2' => '', 'description' => '20:00', 'active' => true],
+            ['code' => 'REDEM', 'value' => '10', 'value_2' => '', 'description' => 'Minimal Redeem Point', 'active' => true],
         ]);
     }
 }
